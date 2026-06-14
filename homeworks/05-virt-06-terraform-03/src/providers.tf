d19f62d7877e85b92261c1b0ad0a4066ec9ed5fa
Gureev Evgeniy <e.gureev@innopolis.ru>
2026-06-14 22:55:39 +0300

    Выполнение задания 4 по занятию 9

--- homeworks/05-virt-06-terraform-03/src/providers.tf
+++ homeworks/05-virt-06-terraform-03/src/providers.tf
@@ -2,6 +2,11 @@ terraform {
   required_providers {
     yandex = {
       source = "yandex-cloud/yandex"
+      version = "0.116.0"
+    }
+    local = {
+      source = "hashicorp/local"
+      version = "2.9.0"
     }
   }
   required_version = "~>1.15.0"
